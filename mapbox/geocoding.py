197c819c569b90440cfc8d97ad15cfa2c7f19166
eddygulled <edgargulay69@gmail.com>
2023-03-29 19:14:22 +0300

    corrections

--- mapbox/geocoding.py
+++ mapbox/geocoding.py
@@ -4,7 +4,7 @@ import keys
 
 YOUR_MAPBOX_ACCESS_TOKEN = keys.API_key
 # provide list of addresses here
-address_list = ['Mbeya', 'morogoro', 'Tanga']
+address_list = ['kigamboni']
 
 for ADDRESS_TO_SEARCH in address_list:
     endpoint = f"https://api.mapbox.com/geocoding/v5/mapbox.places/{ADDRESS_TO_SEARCH}.json?country=tz&proximity=-73.990593%2C40.740121&access_token={YOUR_MAPBOX_ACCESS_TOKEN}"
@@ -15,7 +15,23 @@ for ADDRESS_TO_SEARCH in address_list:
         place_name = feature.get("place_name")
         place_geometry = feature.get("geometry")
         coordinates = place_geometry.get("coordinates")
+        place_type = feature.get("place_type", False)
 
-        # print place name, longitude, latitude
-        response_list = [place_name, coordinates[0], coordinates[1]]
-        print(response_list)
+    boundary = [0,0,0,0]
+    center = [0,0]
+
+    if place_type:
+        if place_type[0] != "locality":
+            boundary = feature.get("bbox")
+            center = feature.get("center")
+
+    # print place name, longitude, latitude
+    response_body = {
+        "place_name": place_name, 
+        "longitude" : coordinates[0], 
+        "latitude" : coordinates[1],
+        "bbox_center" : center,
+        "boundary_square_matrix" : boundary
+    }
+
+    print(f"{ADDRESS_TO_SEARCH}\n", response_body)
